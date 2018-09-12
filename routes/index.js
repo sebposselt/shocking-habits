1593623b144c0c92cbdb7bfc85f2831993bbc99b
Sebastian P <npm981@ku.dk>
2018-09-12 17:05:32 +1000

    new approach

--- routes/index.js
+++ routes/index.js
@@ -1,54 +1,80 @@
 const request = require('request');
 const express = require('express');
 const quakeAPI = require("../lib/QuakeAPI");
+const apiKey = "7f6d6af01b654998bede1ea159f7f3b7"
+const NewsAPI = require('newsapi');
 const newsAPI = require("../lib/NewsAPI");
+const newsapi = new NewsAPI(apiKey);
 const global = require("../lib/Global");
+var bluebird = require("bluebird")
 const router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    let urlObj = { url:quakeAPI.default_query(), json: true }
-    request(urlObj, function (error, response, body) {
-        if (!error && response.statusCode === 200) {
-            //// dev
-            //let newsArticles = newsAPI.itr(body);
-            console.log("ATTENTION: ",newsArticles.length);
-            for (let i = 0; i < newsArticles.length; i++) {
-                const elem = newsArticles[i];
-                console.log((elem.articles).length);
-                console.log(global.DATA_READY)
-            }
-            /// 
-            res.render('index', { 
-                title: "Shocking Habits", 
-                quakes: JSON.stringify(quakeAPI.JsonToCoordArr(body)) 
-            });
-            //development
-            console.log(body.metadata.totalCount);
-        }
-        else res.render('error', { error });
-    });
+	let urlObj = { url: quakeAPI.default_query(), json: true }
+	request(urlObj, function (error, response, body) {
+		if (!error && response.statusCode === 200) {
+			//// dev
+
+			var quakeJsonObjs = newsAPI.itr(body);
+			return bluebird.map(quakeJsonObjs, (quake) => {
+				quake.page = 1;
+				console.log("in the each with quake:", quake);
+				return newsapi.v2.everything(quake)
+					.then(response => {
+						//dev
+						console.log("in the then")
+						console.log("response:")
+						console.log(response)
+						if (response.status !== "ok") {
+							console.log("newsapi error: ", response)
+						}
+						if ((response.articles).length !== 0) {
+							//dev
+							console.log("##############length################### : ", (response.articles).length);
+							this.articlesTotal = response.totalResults;
+							return response.articles;
+						}
+					});
+			}).then((articles) => {
+				console.log("ATTENTION: ", articles.length);
+				console.log("ATTENTION, ARTICLES AFTER MAP:");
+				console.log(JSON.stringify(articles))
+				/// 
+				console.log(body.metadata.totalCount);
+				res.render('index', {
+					title: "Shocking Habits",
+					quakes: JSON.stringify(quakeAPI.JsonToMarker(body, articles))
+				});
+			});
+			//development
+		}
+		else {
+			console.log("request error: ", error);
+			res.render('error', { error });
+		}
+	});
 });
 
-router.post("/", function (req, res) {    
-    //parse what is posted to an obj
-    let userQuery = {};
-    for (let i = 0; i < Object.keys(req.body).length; i++) {
-        let tmp = Object.keys(req.body)[i];
-        if (req.body[tmp] !== '') {
-            userQuery[tmp] = req.body[tmp]
-        }
-    }
+router.post("/", function (req, res) {
+	//parse what is posted to an obj
+	let userQuery = {};
+	for (let i = 0; i < Object.keys(req.body).length; i++) {
+		let tmp = Object.keys(req.body)[i];
+		if (req.body[tmp] !== '') {
+			userQuery[tmp] = req.body[tmp]
+		}
+	}
 
-    let urlObj = { url: quakeAPI.Qconst(userQuery), json: true };
-    request(urlObj, function (error, response, body) {
-        if (!error && response.statusCode === 200) {
-            res.render('index', { title: "Shocking Habits", quakes: JSON.stringify(quakeAPI.JsonToCoordArr(body)) });
-            //development
-            console.log(body.metadata.totalCount);
-        }
-        else res.render('error', { error }); 
-    });
+	let urlObj = { url: quakeAPI.Qconst(userQuery), json: true };
+	request(urlObj, function (error, response, body) {
+		if (!error && response.statusCode === 200) {
+			res.render('index', { title: "Shocking Habits", quakes: JSON.stringify(quakeAPI.JsonToCoordArr(body)) });
+			//development
+			console.log(body.metadata.totalCount);
+		}
+		else res.render('error', { error });
+	});
 });
 
 module.exports = router;
